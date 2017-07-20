1484f28bb70694385b71d56db1a1d62418527f07
Marco De Leon <kjstr4859@gmail.com>
2017-07-20 20:30:42 +0000

    rootrwby

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -7,6 +7,13 @@ def show
 def edit
     @todo=Todo.find_by_id(params[:id])
 end
+def update 
+    t = Todo.find_by_id(params[:id]) 
+    t.description = params['description'] 
+    t.pomodoro_estimate = params['pomodoro-estimate'] 
+    t.save 
+    redirect_to "/todo/show/#{t.id}" 
+end
 def delete
 end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-get'todo/index',to:'todo#index'
+root to:'todo#index'
 get'todo/show/:id',to:'todo#show'
 get'todo/edit/:id',to:'todo#edit'
+get'todo/update/:id', to: 'todo#update'
 end
