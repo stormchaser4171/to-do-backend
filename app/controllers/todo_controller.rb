3c27618927abffc89627ff0d58d64e275901924a
Marco De Leon <kjstr4859@gmail.com>
2017-07-20 17:29:11 +0000

    betterizedrwby

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,7 +2,13 @@ class TodoController < ApplicationController
 def index
 end
 def show
-    @todo_description= "Lunch"
-    @todo_pomodoro_estimate= 2
-end
+    todo_id=params[:id]
+    if todo_id=="1"
+        @todo_description="Lunch"
+        @todo_pomodoro_estimate=2
+        elsif todo_id=="2"
+        @todo_description="Back to Work"
+        @todo_pomodoro_estimate= 50
+        end
+    end
 end
