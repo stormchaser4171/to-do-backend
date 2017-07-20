b97b1a464afdd94ddc83800def67acd54e515f25
Marco De Leon <kjstr4859@gmail.com>
2017-07-20 19:05:36 +0000

    databasecompleterwby

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,13 +2,6 @@ class TodoController < ApplicationController
 def index
 end
 def show
-    todo_id=params[:id]
-    if todo_id=="1"
-        @todo_description="Lunch"
-        @todo_pomodoro_estimate=2
-        elsif todo_id=="2"
-        @todo_description="Back to Work"
-        @todo_pomodoro_estimate= 50
-        end
+    @todo=Todo.find_by_id(params[:id])
     end
 end
